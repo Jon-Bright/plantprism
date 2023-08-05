687edb2537ceb127ddf6e4e7285c7ca54f329443
Jon Bright <jon@siliconcircus.com>
2023-08-05 11:20:44 +0200

    We'll be wanting to use the loggers, keep them around in the object.

--- src/mqtt/mqtt.go
+++ src/mqtt/mqtt.go
@@ -14,6 +14,7 @@ import (
 
 type MQTT struct {
 	c   paho.Client
+	log *logs.Loggers
 }
 
 type brokerFlags struct {
@@ -95,6 +96,6 @@ func New(l *logs.Loggers) (*MQTT, error) {
 		return nil, token.Error()
 	}
 
-	m := MQTT{c}
+	m := MQTT{c, l}
 	return &m, nil
 }
