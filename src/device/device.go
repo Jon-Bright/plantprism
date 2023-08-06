188db52ec4b9e7d3a13f314ee3959faad7952366
Jon Bright <jon@siliconcircus.com>
2023-08-06 17:47:55 +0200

    Parsing's getting pretty big (especially its tests), move to its own file.

--- src/device/device.go
+++ src/device/device.go
@@ -1,11 +1,8 @@
 package device
 
 import (
-	"bytes"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 )
 
 type DeviceMode int
@@ -83,50 +80,11 @@ func (d *Device) processMessage(msg *msgUnparsed) error {
 	return nil
 }
 
-func pickyUnmarshal(data []byte, v any) error {
-	d := json.NewDecoder(bytes.NewReader(data))
-	d.DisallowUnknownFields()
-	err := d.Decode(v)
-	if err != nil {
-		return err
-	}
-	// The data should be one object and nothing more
-	if t, err := d.Token(); err != io.EOF {
-		return fmt.Errorf("trailing data after decode: %T / %v, err %v", t, t, err)
-	}
-	return nil
-}
-
 // Example: {"prev_mode": 0,"mode": 8, "trigger": 1}
 type msgAglMode struct {
 	PrevMode *DeviceMode `json:"prev_mode"`
 	Mode     *DeviceMode
 	Trigger  *ModeTrigger
-}
-
-func parseAglMode(msg *msgUnparsed) (*msgAglMode, error) {
-	var m msgAglMode
-	err := pickyUnmarshal(msg.content, &m)
-	if err != nil {
-		return nil, err
-	}
-	if m.PrevMode == nil {
-		return nil, errors.New("No prev_mode field")
-	} else if m.Mode == nil {
-		return nil, errors.New("No mode field")
-	} else if m.Trigger == nil {
-		return nil, errors.New("No trigger field")
-	} else if *m.PrevMode < ModeDefault || *m.PrevMode >= ModeOutOfRange {
-		return nil, fmt.Errorf("PrevMode %d is invalid", *m.PrevMode)
-	} else if *m.Mode < ModeDefault || *m.Mode >= ModeOutOfRange {
-		return nil, fmt.Errorf("Mode %d is invalid", *m.Mode)
-	} else if *m.Mode == *m.PrevMode {
-		return nil, fmt.Errorf("Mode %d is the same as previously", *m.Mode)
-	} else if *m.Trigger < ModeTriggerApp || *m.Trigger >= ModeTriggerOutOfRange {
-		return nil, fmt.Errorf("Trigger %d is invalid", *m.Trigger)
-	}
-
-	return &m, nil
 }
 
 func (d *Device) processAglMode(msg *msgUnparsed) error {
@@ -151,20 +109,6 @@ type msgAglShadowUpdate struct {
 	State msgAglShadowUpdateState
 }
 
-func parseAglShadowUpdate(msg *msgUnparsed) (*msgAglShadowUpdate, error) {
-	var m msgAglShadowUpdate
-	err := pickyUnmarshal(msg.content, &m)
-	if err != nil {
-		return nil, err
-	}
-	if m.State.Reported.Connected == nil && m.State.Reported.EC == nil {
-		return nil, errors.New("no fields set")
-	}
-	// Never seen them both set at the same time, but there's no
-	// real reason they shouldn't be, so not checking that.
-	return &m, nil
-}
-
 func (d *Device) processAglShadowUpdate(msg *msgUnparsed) error {
 	m, err := parseAglShadowUpdate(msg)
 	if err != nil {
@@ -174,30 +118,14 @@ func (d *Device) processAglShadowUpdate(msg *msgUnparsed) error {
 	return nil
 }
 
-// No parsing: the only time we see this, it has no content
 func (d *Device) processAglShadowGet(msg *msgUnparsed) error {
+	// No parsing: the only time we see this, it has no content
 	return nil
 }
 
 // Example: {"clientToken":"5975bc44"}
 type msgAWSShadowGet struct {
 	ClientToken *string
-}
-
-func parseAWSShadowGet(msg *msgUnparsed) (*msgAWSShadowGet, error) {
-	var m msgAWSShadowGet
-	err := pickyUnmarshal(msg.content, &m)
-	if err != nil {
-		return nil, err
-	}
-	if m.ClientToken == nil {
-		return nil, errors.New("no ClientToken")
-	} else if len(*m.ClientToken) < 8 {
-		return nil, fmt.Errorf("ClientToken '%s' too short", *m.ClientToken)
-	}
-	// Could theoretically check if it's hex, which the
-	// Plantcube's all are, but do we care?
-	return &m, nil
 }
 
 func (d *Device) processAWSShadowGet(msg *msgUnparsed) error {
@@ -242,81 +170,6 @@ type msgAWSShadowUpdateState struct {
 type msgAWSShadowUpdate struct {
 	ClientToken *string
 	State       msgAWSShadowUpdateState
-}
-
-func (m *msgAWSShadowUpdateReported) empty() bool {
-	return m.Cooling == nil && m.Door == nil && m.FirmwareNCU == nil && m.HumidA == nil && m.HumidB == nil &&
-		m.LightA == nil && m.LightB == nil && m.RecipeID == nil && m.TankLevel == nil &&
-		m.TankLevelRaw == nil && m.TempA == nil && m.TempB == nil && m.TempTank == nil &&
-		m.TotalOffset == nil && m.Valve == nil && m.WifiLevel == nil
-}
-
-func (m *msgAWSShadowUpdateReported) validate() error {
-	if m.empty() {
-		return errors.New("update is empty")
-	}
-	if m.FirmwareNCU != nil && *m.FirmwareNCU < 1667466618 {
-		return fmt.Errorf("NCU firmware too old: %d", *m.FirmwareNCU)
-	}
-	if m.HumidA != nil && (*m.HumidA < 30 || *m.HumidA > 100) {
-		// 30% humidity isn't technically an error, but it
-		// sure would be surprising
-		return fmt.Errorf("humidity A out of range: %d", *m.HumidA)
-	}
-	if m.HumidB != nil && (*m.HumidB < 30 || *m.HumidB > 100) {
-		// 30% humidity isn't technically an error, but it
-		// sure would be surprising
-		return fmt.Errorf("humidity B out of range: %d", *m.HumidB)
-	}
-	if m.RecipeID != nil && *m.RecipeID != 1 && *m.RecipeID < 1680300000 {
-		// 1680300000 is 2023-04-01. The recipe shouldn't be that old.
-		// 1 is a flag value the Plantcube uses to request a recipe.
-		return fmt.Errorf("recipe ID invalid: %d", *m.RecipeID)
-	}
-	if m.TankLevel != nil && (*m.TankLevel < 0 || *m.TankLevel > 2) {
-		return fmt.Errorf("tank level invalid: %d", *m.TankLevel)
-	}
-	if m.TankLevelRaw != nil && (*m.TankLevelRaw < 0 || *m.TankLevelRaw > 2) {
-		return fmt.Errorf("raw tank level invalid: %d", *m.TankLevelRaw)
-	}
-	if m.TempA != nil && (*m.TempA < 10.0 || *m.TempA > 40.0) {
-		return fmt.Errorf("temp A out of range: %.1f", *m.TempA)
-	}
-	if m.TempB != nil && (*m.TempB < 10.0 || *m.TempB > 40.0) {
-		return fmt.Errorf("temp B out of range: %.1f", *m.TempB)
-	}
-	if m.TempTank != nil && (*m.TempTank < 10.0 || *m.TempTank > 40.0) {
-		return fmt.Errorf("temp tank out of range: %.1f", *m.TempTank)
-	}
-	if m.TotalOffset != nil && (*m.TotalOffset < 0 || *m.TotalOffset > 86400) {
-		// 86400 = 1 day in seconds. Offset shouldn't exceed this.
-		return fmt.Errorf("total offset out of range: %d", *m.TotalOffset)
-	}
-	if m.Valve != nil && *m.Valve != ValveOpenLayerB && *m.Valve != ValveOpenLayerA && *m.Valve != ValveClosed {
-		return fmt.Errorf("valve value invalid: %d", *m.Valve)
-	}
-	if m.WifiLevel != nil && (*m.WifiLevel < 0 || *m.WifiLevel > 2) {
-		return fmt.Errorf("wifi level invalid: %d", *m.WifiLevel)
-	}
-	return nil
-}
-
-func parseAWSShadowUpdate(msg *msgUnparsed) (*msgAWSShadowUpdate, error) {
-	var m msgAWSShadowUpdate
-	err := json.Unmarshal(msg.content, &m)
-	if err != nil {
-		return nil, err
-	}
-	if m.ClientToken == nil {
-		return nil, errors.New("no ClientToken")
-	} else if len(*m.ClientToken) < 8 {
-		return nil, fmt.Errorf("ClientToken '%s' too short", *m.ClientToken)
-	}
-	err = m.State.Reported.validate()
-	if err != nil {
-		return nil, err
-	}
-	return &m, nil
 }
 
 func (d *Device) processAWSShadowUpdate(msg *msgUnparsed) error {
