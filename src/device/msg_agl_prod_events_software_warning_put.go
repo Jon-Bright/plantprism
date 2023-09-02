ebf1c40e4cdd1a9238117fce9f79e3eb93338a73
Jon Bright <jon@siliconcircus.com>
2023-09-02 17:30:14 +0200

    Properly process the new type of warning. Log unknown types of warning.

--- src/device/msg_agl_prod_events_software_warning_put.go
+++ src/device/msg_agl_prod_events_software_warning_put.go
@@ -47,9 +47,15 @@ func (d *Device) processAglEventWarning(msg *msgUnparsed) error {
 	if err != nil {
 		return err
 	}
-	log.Warn.Printf("Plantcube warning, time %s, label '%s', function '%s', log '%s'", time.Unix(int64(*m.Timestamp), 0).String(), *m.Label, *m.Payload.FunctionName, *m.Payload.ErrorLog)
-	// TODO: When we get an MCU_MODE_STATE, we should warn the
-	// frontend about the door being open
+	if *m.Label == "NCU_SYS_LOG" {
+		log.Warn.Printf("Plantcube syslog warning, time %s, label '%s', function '%s', log '%s'", time.Unix(int64(*m.Timestamp), 0).Local().String(), *m.Label, *m.Payload.FunctionName, *m.Payload.ErrorLog)
+	} else if *m.Label == "MCU_MODE_STATE" {
+		// TODO: When we get an MCU_MODE_STATE, we should warn
+		// the frontend about the door being open
+		log.Warn.Printf("Plantcube mode warning, time %s, label '%s', mode '%s', state '%s', layer '%s'", time.Unix(int64(*m.Timestamp), 0).Local().String(), *m.Label, *m.Payload.Mode, *m.Payload.State, *m.Payload.Layer)
+	} else {
+		log.Error.Printf("Unknown Plantcube warning! time %s, label '%s', raw '%s'", time.Unix(int64(*m.Timestamp), 0).Local().String(), *m.Label, string(msg.content))
+	}
 
 	return nil
 }
